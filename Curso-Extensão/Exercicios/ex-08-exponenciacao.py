ac39514716592ff669d156212792ba424184ae49
skypse <skypse0136@gmail.com>
2024-05-05 18:29:43 -0300

    alteracao

--- Curso-Extensão/Exercicios/ex-08-exponenciacao.py
+++ Curso-Extensão/Exercicios/ex-08-exponenciacao.py
@@ -1,6 +1,20 @@
-# Programa de Exponenciação
+# Curso Básico de Python
+# Nome: Gabriel do Amaral Alves
+# Versão 1.0
+# Exercício de Lógica de Programação utilizando Python
+# Enunciado: Escreva um algoritmo para ler dois valores do teclado (base e exponenciação) e exibir como resposta a exponenciação.
+
+# Programa de calculo da área de um 'trapezio'
+
+# (Saída) - Apresentação
 print("Programa de Exponenciação")
+
+# (Entrada) - Pedindo dados e armazenando
 num1 = int(input("Digite a base: "))
 num2 = int(input("Digite o expoente: "))
+
+# (Processando) - Fazendo o calculo 
 resultado = num1 ** num2
+
+# (Saída) - Exibindo os resultados
 print("O resultado da exponenciação foi: ", resultado)
