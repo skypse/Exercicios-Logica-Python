a4f176264105a9aaceab0da89a555154f8a99d1e
skypse_ <skypse0136@gmail.com>
2024-05-11 10:34:46 -0300

    adicionando exemplo ex 5 do w3schools, e um short hand com if else

---
+++ W3schools/If_else/if_example_ex-05-test_AND.py
@@ -0,0 +1,8 @@
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+    print("Ambas condições são true, ficando A > B e C > A:")
+    print("200 > 33 = TRUE")
+    print("500 > 33 = TRUE")
+# o 'And' é um operador lógico que é usado para combinar condiçoes, logo a primeira e a segunda condição tem que ser 'true' para conseguir ser aprovado!
---
+++ W3schools/If_else/short_hand_if_else_if.py
@@ -0,0 +1,4 @@
+a = 330
+b = 330
+print("A maior que B") if a > b else print(
+    "A e B são iguais") if a == b else print("B maior que A")
