e4882420a0f9a351aac6b4b8f04882007de4f4e4
skypse_ <skypse0136@gmail.com>
2024-06-01 12:22:57 -0300

    adicionando exemplo for loops encadeado 01

--- W3schools/For_loops/Loops_Encadeados/for_loops_encadeado_01.py
+++ W3schools/For_loops/Loops_Encadeados/for_loops_encadeado_01.py
@@ -2,5 +2,5 @@ adj = ["red", "big", "tasty"]
 fruits = ["apple", "banana", "cherry"]
 
 for x in adj:
-    for y in fruits:
+    for y in fruits: # esse loop será executado até o final para que o loop de fora seja executado novamente e entre em outro loop
         print(x, y)
