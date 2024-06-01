cf5174c62b0a17cd397574be534bdae727cf45d7
skypse_ <skypse0136@gmail.com>
2024-06-01 09:12:22 -0300

    modificando o while loops statement

--- W3schools/While_Loops/while_loops_else_statement.py
+++ W3schools/While_Loops/while_loops_else_statement.py
@@ -3,4 +3,4 @@ while i < 6: # loop executa até essa condição se tornar falsa
     print(i)
     i += 1
 else: # imprimir uma mensagem quando a condição for falsa:
-    print("i is no longer less than 6")
+    print("'i' já não é menor a 6")
