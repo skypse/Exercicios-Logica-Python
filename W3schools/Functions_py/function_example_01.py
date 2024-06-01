723872eb8ee1a48732ce0d72b6fd2dbac737b031
skypse_ <skypse0136@gmail.com>
2024-06-01 12:36:38 -0300

    adcionando o exemplo 01 de function em python

---
+++ W3schools/Functions_py/function_example_01.py
@@ -0,0 +1,8 @@
+#Criando uma função. Em Python, uma função é definida usando a palavra-chave def:
+
+def my_function():
+    print("Hello from a function")
+    
+#Chamada de uma função. Para chamar uma função, utilize o nome da função seguido de parêntesis 'my_function()'
+
+my_function()
