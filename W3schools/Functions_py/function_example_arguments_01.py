bc78c22977f8e26bc233187fb187c2ba9698c64a
skypse_ <skypse0136@gmail.com>
2024-06-01 12:40:54 -0300

    adicionando exemplo de passagem de parametro da funcao

---
+++ W3schools/Functions_py/function_example_arguments_01.py
@@ -0,0 +1,13 @@
+# Argumentos
+# As informações podem ser passadas para as funções como argumentos.
+
+# Os argumentos são especificados após o nome da função, dentro de parênteses. Pode adicionar tantos argumentos quantos quiser, basta separá-los com uma vírgula.
+
+#O exemplo seguinte tem uma função com um argumento (fname). Quando a função é chamada, passamos um primeiro nome, que é utilizado dentro da função para imprimir o nome completo:
+
+def my_function(fname):
+    print(fname + " do Amaral")
+
+my_function("Gabriel")
+my_function("Eduardo")
+my_function("Guilherme")
