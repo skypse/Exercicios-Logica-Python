72ced6cfc11d00ddab16beb1debacb1eefdf7c1c
skypse_ <skypse0136@gmail.com>
2024-06-01 09:11:29 -0300

    renomeando o md do statement continue, e adicionando o arquivo while loops com statement else

---
+++ W3schools/While_Loops/while_loops_else_statement.py
@@ -0,0 +1,6 @@
+i = 1
+while i < 6: # loop executa até essa condição se tornar falsa
+    print(i)
+    i += 1
+else: # imprimir uma mensagem quando a condição for falsa:
+    print("i is no longer less than 6")
