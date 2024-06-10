ad615d8720e12d034c988617f6ecf4b6a59f9d24
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-10 13:52:28 -0300

    Adicionando exercicio 03 laco while

---
+++ Curso-Extensão/Exercicios_laco_while/ex-03_Somatoria-Dos-Valores-Pares-Na-Faixa-1-A-500.py
@@ -0,0 +1,19 @@
+# Curso Básico de Python
+# Nome: Gabriel do Amaral Alves
+# Versão 1.0
+# Exercício de Lógica de Programação utilizando Python
+# Enunciado: 3) Elaborar um programa que apresente no final o somatório dos valores pares existentes na faixa de 1 até 500.
+
+# Apresentação Programa
+print("Programa que apresente no final o somatório dos valores pares existentes na faixa de 1 até 500")
+
+# Processamento
+numero = 1
+contadora = 0
+
+while numero <= 500:
+  if (numero % 2 == 0):
+    contadora += numero
+  numero += 1
+
+print(contadora)
