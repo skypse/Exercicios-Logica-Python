f1dc4ccaca28f65470f8e2e8db5dcbe20c663a25
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-10 14:07:44 -0300

    Adicionando exercicio 07 laco while

---
+++ Curso-Extensão/Exercicios_laco_while/ex-07_Media-Aritmetica-Valores-Pares-Faixa-De-50-A-70.py
@@ -0,0 +1,28 @@
+# Curso Básico de Python
+# Nome: Gabriel do Amaral Alves
+# Versão 1.0
+# Exercício de Lógica de Programação utilizando Python
+# Enunciado: 7) Elaborar um programa que apresente os resultados da soma e da média aritmética dos valores pares
+# situados na faixa numérica de 50 a 70.
+
+# Apresentação do Programa
+print("Soma e média aritmética dos valores pares na faixa de 50 a 70")
+
+# Inicialização das variáveis
+soma = 0
+contador = 0
+numero = 50
+
+# Processamento dos valores
+while numero <= 70:
+    if numero % 2 == 0:  # Verifica se o número é par
+        soma += numero
+        contador += 1
+    numero += 1
+
+# Cálculo da média
+media = soma / contador if contador != 0 else 0
+
+# Apresentação dos resultados
+print(f"Soma dos valores pares: {soma}")
+print(f"Média aritmética dos valores pares: {media:.2f}")
