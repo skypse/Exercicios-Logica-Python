55599c08d28d858f7aa5c0d08358896693e860d4
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-13 19:43:17 -0300

    Adicionando exercicio 06

---
+++ Curso-Extensão/Exercicios_Desafio_Python/ex-06_Verficador-Ano-Bissexto.py
@@ -0,0 +1,14 @@
+# Curso Básico de Python
+# Nome: Gabriel do Amaral Alves
+# Versão 1.0
+# Exercício de Lógica de Programação utilizando Python
+# Enunciado: Escreva um programa que leia um ano e verifique se ele é bissexto.
+
+# leitura do ano
+ano = int(input("Digite o ano: "))
+
+# verificação se o ano é bissexto
+if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
+    print(f"O ano {ano} é bissexto.")
+else:
+    print(f"O ano {ano} não é bissexto.")
